684ec274a4a3f370160585076dea003fb1d6a0de
dzaharenko <dimasneg18215>
2019-12-14 15:03:47 +0300

    lesson2

---
+++ js/main.js
@@ -0,0 +1,50 @@
+// №1
+let manager = 'Вася';
+let name = 'Вася';
+let currentUser = 'Вася';
+
+console.log(`Задание 1`);
+console.log(currentUser);
+
+// №2
+console.log(`Задание 2`);
+console.log( `hello ${1}` );
+console.log( `hello ${"name"}` );
+console.log( `hello ${name}` );
+
+// №3
+console.log(`Задание 3`);
+console.log(" \n \n" - 2);
+console.log(true + false);
+console.log(false + false);
+console.log(6 / "3");
+console.log("2" * "3");
+console.log(7 / 0);
+console.log(4 + 5 + "px");
+console.log("" + 3 + 0);
+console.log("" - 4 + 0);
+console.log("1" - 2);
+console.log("3px" - 2);
+console.log("  -9  " + 5);
+console.log("  -9  " - 5);
+console.log(null + 1);
+console.log(undefined + 1);
+
+// №4
+console.log(`Задание 4`);
+console.log(2 > 4);
+console.log(undefined == null);
+console.log(undefined === null);
+console.log(false == 0);
+console.log(false == "");
+console.log(false === "");
+
+// №5
+console.log(`Задание 5`);
+const animal = {
+    typeof: 'dog',
+    color: 'white',
+    speed: '20',
+}
+
+console.log(animal);

9fe8e227e51a9a4f549f8d1947fc926ffc979937
dzaharenko <dimasneg18215>
2020-01-07 14:39:37 +0300

    lesson3

--- js/main.js
+++ js/main.js
@@ -1,56 +1,37 @@
-// №1
-// let manager = 'Вася';
-// let name = 'Вася';
-// let currentUser = 'Вася';
-console.log(`Задание 1`);
-let manager, name, currentUser;
-name = 'Вася';
-manager = name;
-currentUser = manager;
+'use strict';
 
-console.log(currentUser);
+const data = [
+    {
+        firstName: 'Ashton',
+        lastName: 'Kutcher',
+        age: 40,
+    },
+    {
+        firstName: 'Bradley',
+        lastName: 'Pitt',
+        age: 54,
+    },
+    {
+        firstName: 'Hannah',
+        lastName: 'Dakota',
+        age: 24,
+    },
+];
 
-// №2
-console.log(`Задание 2`);
-console.log( `hello ${1}` );
-console.log( `hello ${"name"}` );
-console.log( `hello ${name}` );
+// const f = data.find(el => el.lastName === 'Kutcher');
+// console.log(f);
 
-// №3
-console.log(`Задание 3`);
-console.log(" \n \n" - 2);
-console.log(true + false);
-console.log(false + false);
-console.log(6 / "3");
-console.log("2" * "3");
-console.log(7 / 0);
-console.log(4 + 5 + "px");
-console.log("" + 3 + 0);
-console.log("" - 4 + 0);
-console.log("1" - 2);
-console.log("3px" - 2);
-console.log("  -9  " + 5);
-console.log("  -9  " - 5);
-console.log(null + 1);
-console.log(undefined + 1);
+document.getElementById('input').addEventListener('change', onChange);
 
-// №4
-console.log(`Задание 4`);
-console.log(2 > 4);
-console.log(undefined == null);
-console.log(undefined === null);
-console.log(false == 0);
-console.log(false == "");
-console.log(false === "");
+function onChange() {
+    const text = document.getElementsByTagName("input")[0];
+    const val = text.value;
 
-// №5
-console.log(`Задание 5`);
-const animal = {
-    typeof: 'dog',
-    weight: '40',
-    color: 'white',
-    speed: '20',
+    data.forEach(function(item) {
+        if (item.lastName.includes(val)) {
+            console.log(`${item.firstName} ${item.lastName}, ${item.age} лет`);
+        } else {
+            console.log('No results found for your request');
+        }
+    });
 }
-delete animal.weight;
-
-console.log(animal);

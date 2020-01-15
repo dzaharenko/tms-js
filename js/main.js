4828ce449fc2fd21fd803202d7fe29722e52df3e
dzaharenko <dimasneg18215>
2020-01-15 13:07:49 +0300

    lesson5

--- js/main.js
+++ js/main.js
@@ -1,59 +1,9 @@
-// classwork
-const palindrome = str => {
-    str = str.toLowerCase();
-    return str === str.split('').reverse().join('');
-}
-
-console.log(palindrome('шалаш'));
-
 // №1
-function min(a,b) {
-    return a < b ? a : b;
-}
-console.log(min(-2, 5));
+function sum(a) {
 
-function max(a,b) {
-    return a > b ? a : b;
-}
-console.log(max(-2, 5));
-
-function min(a,b) {
-    if (a < b) {
-        return a;
-    } else {
-        return b;
+    return function(b) {
+        return a + b;
     }
 }
-console.log(min(-2, 5));
 
-function max(a,b) {
-    if (a > b) {
-        return a;
-    } else {
-        return b;
-    }
-}
-console.log(max(-2, 5));
-
-// №2
-let arr = [12, 53, 20, 18, 22, 100, 43, 57, 50, 1];
-let newArr = [];
-
-function ThisArr(arr) {
-    arr.forEach(function (item, i, array) {
-        if (item == 0) {
-            newArr.push('zero');
-        } else if (item % 100 == 0) {
-            newArr.push(item / 100 + 'zerozero');
-        } else if (item % 10 == 0) {
-            newArr.push(item / 10 + 'zero');
-        } else {
-            newArr.push(item);
-        }
-    });
-    return newArr;
-}
-
-ThisArr(arr);
-console.log(arr);
-console.log(newArr);
+console.log(sum(5)(2));
